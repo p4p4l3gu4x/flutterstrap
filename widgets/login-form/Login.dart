7054a2da3f743616c355d990a4f9107505e02e74
Hudson Martins <hudson.santos@broadcom.com>
2019-06-01 16:52:51 +0200

    Beutify the layout of login page

--- widgets/login-form/Login.dart
+++ widgets/login-form/Login.dart
@@ -15,26 +15,42 @@ class LoginWidget extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              Image.asset(imageSrc),
+              Container(
+                width: 300,
+                height: 250,
+                child: Image.asset(imageSrc),
+              ),
               LoginInputFieldWidget(Icon(Icons.person, color: Colors.white), "User Name"),
               LoginInputFieldWidget(Icon(Icons.lock, color: Colors.white), "Password"),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
                   Container(
-                    child: RaisedButton(
-                      onPressed: (){},
-                      color: Colors.blue,
-                      textColor: Colors.white,
-                      child: Text('Login'),
+                    child: SizedBox(
+                      width: 100,
+                      height: 60,
+                      child: RaisedButton(
+                        onPressed: (){},
+                        color: Colors.blue,
+                        textColor: Colors.white,
+                        child: Text('Login', style: TextStyle(fontSize: 20.0)),
+                        padding: const EdgeInsets.all(8.0),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                      ),
                     ),
                   ),
                   Container(
-                    child: RaisedButton(
-                      onPressed: (){},
-                      color: Colors.blue,
-                      textColor: Colors.white,
-                      child: Text('Forgout'),
+                    child: SizedBox(
+                      width: 100,
+                      height: 60,
+                      child: RaisedButton(
+                        onPressed: (){},
+                        color: Colors.blue,
+                        textColor: Colors.white,
+                        child: Text('Forgot', style: TextStyle(fontSize: 20.0)),
+                        padding: const EdgeInsets.all(8.0),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                      ),
                     ),
                   )
                 ],
